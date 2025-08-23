b53332902ab8d9c13e2b09e2417124494ed5b68e
EddieWjy <3040953216@qq.com>
2025-08-23 13:54:29 +0800

    跨域处理

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 // src/server.ts
 import express from "express";
+import cors from "cors";
 import { WebSocketServer } from "ws";
 import { SingleMatch } from "./singleMatch";
 
 const app = express();
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:3000"],
+  credentials: true
+}));
 app.use(express.json());
 
 const defaultBots = [
